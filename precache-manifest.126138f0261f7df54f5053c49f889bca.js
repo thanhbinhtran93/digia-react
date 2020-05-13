9ea9022fb79ffc0062a327c2257d280c45c25e3c
Binh Tran <thanhbinh.tran93@gmail.com>
2020-05-13 15:01:18 +0300

    Updates

---
+++ precache-manifest.126138f0261f7df54f5053c49f889bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0620d990983ea93264a87f21b8788d2",
+    "url": "/digia-react/index.html"
+  },
+  {
+    "revision": "7140b79932f857c95112",
+    "url": "/digia-react/static/js/2.e1020502.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/digia-react/static/js/2.e1020502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac65c43e1f3915cce5e5",
+    "url": "/digia-react/static/js/main.0fa602ed.chunk.js"
+  },
+  {
+    "revision": "3ae0eb4912ee894970e0",
+    "url": "/digia-react/static/js/runtime-main.93639f20.js"
+  }
+]);
