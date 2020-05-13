42b4c3eec7b94a03a0f9fbc884385ea888e43034
Binh Tran <thanhbinh.tran93@gmail.com>
2020-05-13 15:09:52 +0300

    Validate email

--- src/vadilators/participant-validator.ts
+++ src/vadilators/participant-validator.ts
@@ -3,7 +3,9 @@ import { Person } from 'interfaces/Person';
 
 export const participantSchema = Yup.object<Person>({
   name: Yup.string().required(),
-  email: Yup.string().required(),
+  email: Yup.string()
+    .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, 'Invalid email')
+    .required(),
   phone: Yup.string()
     .matches(/^\d{10}$/, 'Phone has to be 10 digits')
     .required(),
